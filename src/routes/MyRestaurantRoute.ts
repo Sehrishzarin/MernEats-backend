80080d796f92c74adc18b6387473f3afa6eab293
SehrishZarin <sehrishzarin45@gmail.com>
2026-07-12 17:16:59 +0500

    update resturant for bug fixes

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -5,12 +5,11 @@ import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
-
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, //5mb
+    fileSize: 5 * 1024 * 1024, // 5mb
   },
 });
 
